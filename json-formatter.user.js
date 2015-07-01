53feb8f3bb392e15ac652e5224b7df72aaccc08f
Gerald <gera2ld@163.com>
2015-07-01 15:46:38 +0800

    show empty collections in one line

--- json-formatter.user.js
+++ json-formatter.user.js
@@ -3,7 +3,7 @@
 // @namespace http://gerald.top
 // @description Format JSON data in a beautiful way.
 // @description:zh-CN 更加漂亮地显示JSON数据。
-// @version 0.1
+// @version 1.0
 // @match *://*/*
 // @grant GM_addStyle
 // @grant GM_registerMenuCommand
@@ -18,28 +18,93 @@ function safeHTML(html) {
   });
 }
 
+function join(list) {
+  var html = [];
+  var open = false;
+  var last = null;
+  var close = function () {
+    html.push('</li>');
+    open = false;
+    last = null;
+  };
+  list.forEach(function (item) {
+    if (open && !item.backwards)
+      close();
+    if (!open) {
+      html.push('<li>');
+      open = true;
+    }
+    if (item.backwards && last && last.forwards)
+      html.push(last.separator);
+    html.push(item.data);
+    if (!item.forwards)
+      close();
+    else
+      last = item;
+  });
+  if (open) html.push('</li>');
+  return html.join('');
+}
+
+function getHtml(data, cls) {
+  return '<span class="' + (cls || typeof data) + '">' + data + '</span>';
+}
+
 function render(data) {
   if (Array.isArray(data)) {
-    var ret = [];
-    ret.push({data: '[', cls: 'opr'});
-    data.forEach(function (item) {
-      ret = ret.concat(render(item));
-    });
-    ret.push({data: ']', cls: 'opr', back: true});
+    var arr = [];
+    var ret = {
+      backwards: true,
+      forwards: true,
+      separator: getHtml(',', 'separator'),
+    };
+    arr.push(getHtml('[', 'operator'));
+    if (data.length) {
+      arr.push('<ul>');
+      arr.push(join(data.map(render)));
+      arr.push('</ul>');
+    } else {
+      arr.push(getHtml('', 'separator'));
+      ret.backwards = ret.forwards = false;
+    }
+    arr.push(getHtml(']', 'operator'));
+    ret.data = arr.join('');
     return ret;
   } else if (data === null)
-    return {data: data.toString(), cls: 'null'};
+    return {data: getHtml('null', 'null'), backwards: true};
   else if (typeof data == 'object') {
-    var ret = [];
-    ret.push({data: '{', cls: 'opr'});
+    var arr = [];
+    var ret = {
+      backwards: true,
+      forwards: true,
+      separator: getHtml(',', 'separator'),
+    };
+    arr.push(getHtml('{', 'operator'));
+    var objdata = [];
     for (var key in data) {
-      ret.push({data: key.toString(), cls: 'key'});
-      ret = ret.concat(render(data[key]));
+      objdata.push({
+        data: getHtml(key, 'key'),
+        forwards: true,
+        separator: getHtml(':', 'separator'),
+      });
+      objdata.push(render(data[key]));
     }
-    ret.push({data: '}', cls: 'opr', back: true});
+    if (objdata.length) {
+      arr.push('<ul>');
+      arr.push(join(objdata));
+      arr.push('</ul>');
+    } else {
+      arr.push(getHtml('', 'separator'));
+      ret.backwards = ret.forwards = false;
+    }
+    arr.push(getHtml('}', 'operator'));
+    ret.data = arr.join('');
     return ret;
   } else
-    return {data: data.toString(), cls: typeof data};
+    return {
+      backwards: true,
+      data: getHtml(data),
+    };
 }
 
 function formatJSON() {
@@ -53,49 +118,15 @@ function formatJSON() {
       config.style = GM_addStyle(
         'ul.root{padding-left:0;}' +
         'li{list-style:none;}' +
-        '.comma,.colon{margin-right:1em;}' +
+        '.separator{margin-right:.5em;}' +
         '.number{color:darkorange;}' +
         '.key{color:brown;}' +
         '.string{color:green;}' +
-        '.opr{color:blue;}'
+        '.operator{color:blue;}'
       );
     }
     var ret = render(config.data);
-    var html = ['<ul class="root">'];
-    var lastCls;
-    var open = false;
-    ret.forEach(function (item) {
-      if (item.cls == 'opr') {
-        if (item.back) html.push('</ul>');
-        if (open) {
-          if(!item.back && lastCls == 'opr')
-            html.push('<span class="comma">,</span>');
-        } else {
-          html.push('<li>');
-          open = true;
-        }
-        html.push('<span class="' + item.cls + '">' + safeHTML(item.data) + '</span>');
-        if (!item.back) {
-          html.push('<ul>');
-          open = false;
-        }
-      } else if (item.cls == 'key') {
-        // assert(open == false);
-        html.push('<li>');
-        open = true;
-        html.push('<span class="key">' + safeHTML(item.data) + '</span>');
-        html.push('<span class="colon">:</span>');
-      } else {
-        if (lastCls != 'key') {
-          html.push('<li>');
-          open = true;
-        }
-        html.push('<span class="' + item.cls + '">' + safeHTML(item.data) + '</span>');
-      }
-      lastCls = item.cls;
-    });
-    html.push('</ul>');
-    document.body.innerHTML = html.join('');
+    document.body.innerHTML = '<ul class="root"><li>' + ret.data + '</li></ul>';
     config.formatted = true;
   }
 }
