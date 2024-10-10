e619db21ef065907a3fe12a53ec9ab7fcffff752
gera2ld <gera2ld@live.com>
2024-10-10 03:09:43 +0000

    Deploying to release from master @ e38dfa15896f3f978ed81b49df13c0438985a12e 🚀

--- json-formatter.user.js
+++ json-formatter.user.js
@@ -5,7 +5,7 @@
 // @icon              http://cn.gravatar.com/avatar/a0ad718d86d21262ccd6ff271ece08a3?s=80
 // @description       Format JSON data in a beautiful way.
 // @description:zh-CN 更加漂亮地显示JSON数据。
-// @version           process.env.VERSION
+// @version           2.0.10
 // @require           https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @match             *://*/*
 // @match             file:///*
