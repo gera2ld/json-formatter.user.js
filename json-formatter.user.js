942988afe1515bc5d1e1b379e068e2cae9e58ae0
gera2ld <gera2ld@live.com>
2024-10-16 04:25:41 +0000

    Deploying to release from master @ eabb99cb31efba8bdd17b7faf95f02bcc10b3e4b 🚀

--- json-formatter.user.js
+++ json-formatter.user.js
@@ -5,22 +5,28 @@
 // @icon              http://cn.gravatar.com/avatar/a0ad718d86d21262ccd6ff271ece08a3?s=80
 // @description       Format JSON data in a beautiful way.
 // @description:zh-CN 更加漂亮地显示JSON数据。
-// @version           2.0.11
+// @version           2.0.12
 // @require           https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @match             *://*/*
 // @match             file:///*
 // @grant             GM_addElement
 // @grant             GM_registerMenuCommand
+// @grant             GM_xmlhttpRequest
 // ==/UserScript==
 
 (function () {
 'use strict';
 
+const JSON_VIEWER_EMBED = 'https://json.pore.run/embed';
 let iframe;
-if (testRules([
-// text/javascript - file:///foo/bar.js
-/^(?:text|application)\/(?:.*?\+)?(?:plain|json|javascript)$/], document.contentType)) formatJSON();
-GM_registerMenuCommand('Format JSON', formatJSON);
+if (window === window.top && window.location.origin + window.location.pathname === JSON_VIEWER_EMBED) {
+  handleViewerAfterRedirection();
+} else {
+  if (testRules([
+  // text/javascript - file:///foo/bar.js
+  /^(?:text|application)\/(?:.*?\+)?(?:plain|json|javascript)$/], document.contentType)) handleViewerIframe();
+  GM_registerMenuCommand('Format JSON', handleViewerIframe);
+}
 function testRules(rules, contentType) {
   for (const rule of rules) {
     if (typeof rule === 'string') {
@@ -31,23 +37,30 @@ function testRules(rules, contentType) {
   }
   return false;
 }
-function formatJSON() {
+function handleViewerIframe() {
   if (iframe) return;
   const content = JSON.parse(document.body.textContent);
   document.body.innerHTML = '';
   iframe = GM_addElement(document.body, 'iframe', {
     sandbox: 'allow-scripts allow-same-origin',
-    src: 'https://json.gera2ld.space/embed',
+    src: JSON_VIEWER_EMBED,
     style: `position:fixed;width:100vw;height:100vh;inset:0;border:none`
   });
+  let initiated = false;
   const setData = () => {
-    iframe.contentWindow.postMessage({
-      type: 'setData',
-      payload: content
-    }, '*');
+    initiated = true;
+    handleInitData(content, iframe.contentWindow);
   };
   iframe.addEventListener('load', () => {
-    setData();
+    setTimeout(() => {
+      if (!initiated) {
+        // JS blocked, redirect to the embed page
+        const url = new URL(JSON_VIEWER_EMBED);
+        url.searchParams.set('from', 'userscript');
+        url.searchParams.set('json_url', window.location.href);
+        window.location.assign(url);
+      }
+    }, 2000);
   });
   window.addEventListener('message', e => {
     const {
@@ -61,6 +74,45 @@ function formatJSON() {
         }
     }
   });
+}
+async function handleViewerAfterRedirection() {
+  const url = new URL(window.location.href);
+  if (url.searchParams.get('from') !== 'userscript') return;
+  const jsonUrl = url.searchParams.get('json_url');
+  const content = await new Promise((resolve, reject) => {
+    GM_xmlhttpRequest({
+      url: jsonUrl,
+      responseType: 'json',
+      onload: res => {
+        resolve(res.response);
+      },
+      onerror: res => {
+        reject(res);
+      }
+    });
+  });
+  handleInitData(content, window, jsonUrl);
+  window.addEventListener('load', () => {
+    handleInitData(content, window, jsonUrl);
+  });
+}
+function handleInitData(content, window, jsonUrl) {
+  window.postMessage({
+    type: 'setReadOnly',
+    payload: true
+  }, '*');
+  window.postMessage({
+    type: 'setData',
+    payload: content
+  }, '*');
+  if (jsonUrl) {
+    window.postMessage({
+      type: 'setBanner',
+      payload: {
+        html: `JSON URL: <a href="${jsonUrl}">${jsonUrl}</a>`
+      }
+    });
+  }
 }
 
 })();
