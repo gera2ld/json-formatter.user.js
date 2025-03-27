e5bcf8538c0433bf165c8f223a0a1b05ed71d3f4
anhkhoakz <113995598+anhkhoakz@users.noreply.github.com>
2025-03-27 08:45:41 +0700

    feat: upgrade https for @icon in meta.js feat: update LICENSE for matche with the repo in package.json

--- src/meta.js
+++ src/meta.js
@@ -2,11 +2,11 @@
 // @name        JSON formatter
 // @namespace   https://gera2ld.space
 // @author      process.env.AUTHOR
-// @icon        http://cn.gravatar.com/avatar/a0ad718d86d21262ccd6ff271ece08a3?s=80
+// @icon        https://cn.gravatar.com/avatar/a0ad718d86d21262ccd6ff271ece08a3?s=80
 // @description Format JSON data in a beautiful way.
 // @description:zh-CN 更加漂亮地显示JSON数据。
 // @version     process.env.VERSION
-// @require https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
+// @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @match       *://*/*
 // @match       file:///*
 // ==/UserScript==
