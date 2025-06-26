d0a5625f28fedd7c9b8d63f17a798f36f450202c
gera2ld <gera2ld@live.com>
2025-06-26 15:08:43 +0000

    Deploying to release from @ gera2ld/json-formatter.user.js@4321a3b518b2033930d6a99e21c6891bd8385279 🚀

--- json-formatter.user.js
+++ json-formatter.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name              JSON formatter
 // @namespace         https://gera2ld.space
-// @author            Gerald <gera2ld@live.com>
-// @icon              http://cn.gravatar.com/avatar/a0ad718d86d21262ccd6ff271ece08a3?s=80
+// @author            Gerald
+// @icon              https://cn.gravatar.com/avatar/a0ad718d86d21262ccd6ff271ece08a3?s=80
 // @description       Format JSON data in a beautiful way.
 // @description:zh-CN 更加漂亮地显示JSON数据。
-// @version           2.0.13
+// @version           process.env.VERSION
 // @require           https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @match             *://*/*
 // @match             file:///*
@@ -42,7 +42,7 @@ function handleViewerIframe() {
   const content = JSON.parse(document.body.textContent);
   document.body.innerHTML = '';
   iframe = GM_addElement(document.body, 'iframe', {
-    sandbox: 'allow-scripts allow-same-origin',
+    sandbox: 'allow-scripts allow-same-origin allow-popups',
     src: JSON_VIEWER_EMBED,
     style: `position:fixed;width:100vw;height:100vh;inset:0;border:none`
   });
